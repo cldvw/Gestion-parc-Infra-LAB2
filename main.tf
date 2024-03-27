7c6b85f270be2502364d1b6b74e92d61362298cd
cldvw <150364820+cldvw@users.noreply.github.com>
2024-03-27 17:38:22 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,55 +1,31 @@
-terraform {
-
-required_providers {
-
-azurerm = {
-
-source = "hashicorp/azurerm"
-
-version = "~> 3.0.0"
-
-}
-
-required_version = ">= 0.14.9"
-
-}
-
-provider "azurerm" {
-
-features {}
-
-# b. Code pour la ressource random_integer
 resource "random_integer" "random_suffix" {
   min = 1000
   max = 9999
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "rg-${var.Ryan}-${random_integer.random_suffix.result}"
-  location = var.location
+  name     = "rg_cloud_shell_Ryan_LE-${random_integer.random_suffix.result}"
+  location = "France Central"
 }
 
 resource "azurerm_app_service_plan" "example" {
-  name                = "asp-${var.Ryan}-${random_integer.random_suffix.result}"
+  name                = "asp_cloud_shell_Ryan_LE-${random_integer.random_suffix.result}"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  os_type             = "Linux"
+  os_type             = "Windows"
   sku {
     tier = "Basic"
     size = "B1"
   }
 }
 
-resource "azurerm_linux_web_app" "example" {
-  name                = "webapp-${var.Ryan}-${random_integer.random_suffix.result}"
+resource "azurerm_app_service" "example" {
+  name                = "webapp_cloud_shell_Ryan_LE-${random_integer.random_suffix.result}"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
   site_config {
-    application_stack {
-      java_server       = "JAVA"
-      java_version      = "java17"
-      java_server_version = "17"
-    }
+    windows_fx_version = "WINDOWS|latest"
   }
 }
-
